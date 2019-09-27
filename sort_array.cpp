351d5ea4c74f134b6c072678052a2c6953596738
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-09-27 10:49:19 -0600

    Add files via upload

---
+++ sort_array.cpp
@@ -0,0 +1,22 @@
+//
+//  sort_array.cpp
+//  sort_array
+//
+//  Created by Warnock, Scott A on 9/27/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void printmessage()
+{
+    cout << "I'm a function that prints a message!";
+}
+
+
+int main()
+{
+    printmessage();
+}
