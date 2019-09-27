628efc661b5de64ea1470b67c4fdbca51704cc05
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-09-27 06:47:06 -0600

    Add files via upload

---
+++ print_number.cpp
@@ -0,0 +1,19 @@
+//
+//  print_number.cpp
+//  print_number
+//
+//  Created by Warnock, Scott A on 9/27/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+    cout << "You entered " << number;
+    return 0;
+}
