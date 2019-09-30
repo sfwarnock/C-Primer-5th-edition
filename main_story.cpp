7253499ad3515f5c699716f7f3b3eef6911b389f
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-09-30 07:45:03 -0600

    Add files via upload

---
+++ main_story.cpp
@@ -0,0 +1,25 @@
+//
+//  main_story.cpp
+//  story
+//
+//  Created by Warnock, Scott A on 9/30/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string character_Name = "Wes";
+    int character_Age;
+    character_Age = 15;
+    
+    cout << "There once was a boy named " << character_Name << endl;
+    cout << "He was " << character_Age << " years old" << endl;
+    cout << "He liked the name " << character_Name << endl;
+    cout << "But he did not like being " << character_Age << endl;
+    
+    return 0;
+}
