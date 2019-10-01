30fc45aba117882dab40adfa694d3a5f39a42823
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-01 07:26:06 -0600

    Add files via upload

---
+++ main.datatype.cpp
@@ -0,0 +1,16 @@
+//
+//  datatype.cpp
+//  datatype
+//
+//  Created by Warnock, Scott A on 10/1/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
