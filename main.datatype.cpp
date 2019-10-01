e65c44992d2767093c87cf74dc83ea5a0b79255f
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-01 08:51:53 -0600

    Add files via upload

--- main.datatype.cpp
+++ main.datatype.cpp
@@ -12,5 +12,12 @@ using namespace std;
 
 int main()
 {
+    char letter = 'T';
+    string word = "Telluride is a village in Colorado";
+    int elevation_feet = 8750;
+    double latitude = 37.9375;
+    double longitude = 107.8123;
+    bool isTelluride = true;
     
+    cout << word << endl;
 }
