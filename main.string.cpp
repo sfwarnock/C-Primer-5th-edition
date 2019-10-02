92c89bbf32f2adbb81263542e5b85fd6add1a5f6
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-02 06:44:13 -0600

    Add files via upload

--- main.string.cpp
+++ main.string.cpp
@@ -1,6 +1,5 @@
 //
-//  string.cpp
-//  strings
+//  main.string.cpp
 //
 //  Created by Warnock, Scott A on 10/1/19.
 //  Copyright © 2019 Warnock, Scott A. All rights reserved.
@@ -13,4 +12,22 @@ using namespace std;
 int main()
 {
     
+    string location = "Telluride, Colorado";
+    cout << location << endl;
+    
+    cout << location.length() << endl;
+    
+    cout << location[0] << endl;
+    cout << location[3] << endl;
+    
+    location[0] = 't';
+    cout << location << endl;
+    
+    cout << location.find("ride", 0) << endl;
+    
+    string location_state;
+    location_state = location.substr(11, 9);
+    cout << location_state << endl;
+    
+    return 0;
 }
