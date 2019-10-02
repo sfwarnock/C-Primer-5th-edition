5fc14bb8689d9410632d6d4f40b42d4e0c739f4c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-02 07:31:39 -0600

    Add files via upload

--- main.numbers.cpp
+++ main.numbers.cpp
@@ -7,10 +7,51 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+    cout << 39 << endl;
+    cout << 39.12 << endl;
+    
+    cout << 14 + 7 << endl;
+    cout << 14 - 7 << endl;
+    cout << 14 * 7 << endl;
+    cout << 14 / 7 << endl;
+    cout << 21 % 10 << endl;
+    
+    cout << 4 + 5 * 10 << endl;
+    cout << (4 + 5) * 10 << endl;
+    
+    int whole_number = 5;
+    double decimal_number = 5.5;
+    
+    whole_number++;
+    cout << whole_number << endl;
+    
+    decimal_number--;
+    cout << decimal_number << endl;
+    
+    whole_number += 80;
+    cout << whole_number << endl;
+    
+    cout<< decimal_number + 5 << endl;
+    
+    cout << pow(5, 2) << endl;
+    cout << pow(3, 3) << endl;
+    
+    cout << sqrt(9) << endl;
+    
+    cout << round(4.6) << endl;
+    
+    cout<< ceil(4.1) << endl;
+    cout<< floor(4.9) << endl;
+    
+    cout<< fmax(3,10) << endl;
+    cout<< fmin(3, 10) << endl;
+    
+    return 0;
     
 }
