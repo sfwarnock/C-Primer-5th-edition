74cadf4c1a079db26a212a5a84dd41404d9a7222
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-07 12:36:43 -0600

    Add files via upload

---
+++ main.mad_lib.cpp
@@ -0,0 +1,17 @@
+//
+//  mad_lib.cpp
+//  madlib
+//
+//  Created by Warnock, Scott A on 10/7/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
