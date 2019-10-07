7a2a9a632789e1eaa82bdd3ad47c83729532eb98
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-07 13:40:14 -0600

    Add files via upload

--- main.mad_lib.cpp
+++ main.mad_lib.cpp
@@ -12,6 +12,18 @@ using namespace std;
 
 int main()
 {
+    string color, plural_Noun, athlete;
+    
+    cout << "Enter a color: " << endl;
+    getline(cin, color);
+    cout << "Enter a plural noun: " << endl;
+    getline(cin, plural_Noun);
+    cout << "Enter in the name of an athlete: " << endl;
+    getline(cin, athlete);
+    
+    cout << "Roses are " << color << endl;
+    cout << plural_Noun << " are blue" << endl;
+    cout << "I love " << athlete << endl;
     
     return 0;
 }
