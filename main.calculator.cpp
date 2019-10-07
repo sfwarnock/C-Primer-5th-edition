27d11fc76f5d14c7321c6193a203f14e49af2b0a
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-07 12:33:10 -0600

    Add files via upload

--- main.calculator.cpp
+++ main.calculator.cpp
@@ -12,8 +12,15 @@ using namespace std;
 
 int main()
 {
+    double num1, num2;
     
+    cout << "Enter the first number " << endl;
+    cin >> num1;
     
+    cout << "Enter the second number " << endl;
+    cin >> num2;
+    
+    cout << num1 + num2 << endl;
     
     return 0;
 }
