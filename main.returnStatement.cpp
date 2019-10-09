a927c3eff3229229f980d1b9dcee8029981a8462
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-09 14:34:58 -0600

    Add files via upload

--- main.returnStatement.cpp
+++ main.returnStatement.cpp
@@ -10,8 +10,15 @@
 
 using namespace std;
 
+double cube(double num)
+{
+    double result = num * num * num;
+    return result;
+}
+
 int main()
 {
-    
+    double answer = cube(5.0);
+    cout << answer << endl;
     return 0;
 }
