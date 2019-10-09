e0bdf418e69c52120338f05925c1ed77df09336c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-09 05:42:13 -0600

    Add files via upload

--- main.functions.cpp
+++ main.functions.cpp
@@ -10,8 +10,16 @@
 
 using namespace std;
 
+
+void sayHi(string name, int age);
+
 int main()
 {
-    
+    sayHi("Mike", 35);
     return 0;
+}
+
+void sayHi(string name, int age)
+{
+    cout << "Hello " << name << " you are " << age << endl;
 }
