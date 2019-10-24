985281008f2a46db55612c4e7690ffa31e859c3b
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-24 11:29:36 -0600

    Add files via upload

--- main.for_calculator.cpp
+++ main.for_calculator.cpp
@@ -11,6 +11,30 @@ using namespace std;
 
 int main()
 {
+    double num1, num2;
+    char oper;
+    
+    cout << "Enter first number: " << endl;
+    cin >> num1;
+    cout << "Enter operator: " << endl;
+    cin >> oper;
+    cout << "Enter second number: " << endl;
+    cin >> num2;
+    
+    double result;
+    
+    if(oper == '+'){
+        result = num1 + num2;
+    } else if(oper == '-'){
+        result = num1 - num2;
+    } else if(oper == '/'){
+        result = num1 / num2;
+    } else if(oper == '*'){
+        result = num1 * num2;
+    } else {
+        cout << "Invalid operator given." << endl;
+    }
+    cout << result << endl;
     
     return 0;
 }
