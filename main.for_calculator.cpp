424979ee799d5ef35f1dd23b8532e052cd841c43
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-24 07:13:27 -0600

    Add files via upload

---
+++ main.for_calculator.cpp
@@ -0,0 +1,9 @@
+//
+//  for_calculator.cpp
+//  for_calculator
+//
+//  Created by Warnock, Scott A on 10/24/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include "for_calculator.hpp"
