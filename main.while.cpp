6b397a35e8b7f7d14d187b11e9b9b1cdb136dac7
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-10-31 12:41:50 -0600

    Add files via upload

---
+++ main.while.cpp
@@ -0,0 +1,17 @@
+//
+//  while.cpp
+//  while.statement
+//
+//  Created by Warnock, Scott A on 10/31/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
