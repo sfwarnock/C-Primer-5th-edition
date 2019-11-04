b2e308cea3a2fdf99361e5ec476a251796b876a0
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-11-04 07:34:25 -0700

    Add files via upload

--- main.while.cpp
+++ main.while.cpp
@@ -12,11 +12,11 @@ using namespace std;
 
 int main()
 {
-    int index = 1;
-    while (index <= 5){
+    int index = 6;
+    do {
         cout << index << endl;
         index++;
-    }
+    } while (index <= 5);
     
     return 0;
 }
