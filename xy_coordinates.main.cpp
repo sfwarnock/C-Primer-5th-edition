0adf2e85b80ce3a7ae845a3be248f0514bf0c25c
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2019-12-02 15:04:15 -0700

    Add files via upload

---
+++ xy_coordinates.main.cpp
@@ -0,0 +1,18 @@
+//
+//  xy_coordinates.main.cpp
+//  xy_coordinates
+//
+//  Created by Warnock, Scott A on 12/2/19.
+//  Copyright © 2019 Warnock, Scott A. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x, y, a, b, m;
+    
+    return 0;
+}
