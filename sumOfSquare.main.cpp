fa53b4d421689b3f8068cbb0114107c6d618162e
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-02-28 10:36:13 -0700

    Add files via upload

--- sumOfSquare.main.cpp
+++ sumOfSquare.main.cpp
@@ -7,4 +7,18 @@
 #include<iostream>
 #include<stdio.h>
 
+int total;
+int square(int x){
+    return x*x;
+}
 
+int squareOfSum(int x, int y){
+    int z = square(x + y);
+    return z;
+}
+
+int main(){
+    int a = 4, b = 4;
+    total = squareOfSum(a,b);
+    printf("output = %d", total);
+}
