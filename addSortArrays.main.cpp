f10b3f791562b2b82c588b9b9bcee3d986343cf4
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-03-04 06:41:17 -0700

    Add files via upload

--- addSortArrays.main.cpp
+++ addSortArrays.main.cpp
@@ -12,16 +12,24 @@ int arrayTwo [5] = {1, 3, 5, 7, 9};
 vector<int> newList;
 
 int sumArrayElements(int arrayOneElement, int arrayTwoElement){
-    int arraySum = arrayOne[arrayOneElement] + arrayTwo[arrayTwoElement];
-    return arraySum;
+    for(int arrayOneElement = 0; arrayOneElement < 5; arrayOneElement++){
+        int arraySum = arrayOne[arrayOneElement] + arrayTwo[arrayTwoElement];
+        return arraySum;
+    }
+    return 0;
 }
 
 int squareSum(int sumOfArray){
+    
     int arraySquared = sumOfArray * sumOfArray;
-    return arraySquared;
+    newList.insert(newList.begin(), arraySquared);
+    return 0;
 }
 
 int main(){
+    for(int i = 0; i < 5; i++){
+        cout<< arrayOne[i] << endl;
+    }
     int indexOne = 0, indexTwo = 0;
     cout << sumArrayElements(indexOne, indexTwo) << endl;
     return 0;
