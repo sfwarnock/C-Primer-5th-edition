c88bf4744882ae5e401e07bbda8ee94dc2949f19
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-03-16 06:32:29 -0600

    Add files via upload

--- playingWithVector.main.cpp
+++ playingWithVector.main.cpp
@@ -10,3 +10,22 @@
 #include <vector>
 
 using namespace std;
+
+vector<int> vectorOne;
+int arrayOne[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+//int arraySize = arrayOne.size();
+
+int fillVector(){
+    int indexOne = 0;
+    while(indexOne < 25){
+        vectorOne.insert(vectorOne.end(), indexOne);
+        indexOne++;
+    }
+    cout << vectorOne.size() << endl;
+    return 0;
+}
+
+
+int main (){
+    fillVector();
+}
