bf18fde710a6e1210e81854452fc809f7a4be701
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2020-03-25 15:06:11 -0600

    Add files via upload

--- playingWithVector.main.cpp
+++ playingWithVector.main.cpp
@@ -12,8 +12,9 @@
 using namespace std;
 
 vector<int> vectorOne;
-vector<int> vectorClean;
-int arrayOne[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+//vector<int> vectorClean;
+//int arrayOne[] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+int arrayOne[] = {5, 4, 3, 2, 1, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
 //int arraySize = arrayOne.size();
 
@@ -51,15 +52,36 @@ int removeDuplicates(){
         cout << "Vector Size Start: " << vectorSizeStart << endl;
         cout << "Removed Elements: " << removedElementsCount << endl;
         cout << "Cleaned Vector Size: " << vectorSizeFinish << endl;
+        cout << " " << endl;
     } else {
-        cout << "Assert Error, remove duplicates from vector failed." << endl;
+        cout << "Assert Error: remove duplicates from vector failed." << endl;
     }
     return 0;
 }
 
 
 int sortVector(){
+    int indexOne = 0;
+    int indexTwo = indexOne + 1;
     
+    int swapElementOne = vectorOne.at(indexOne);
+    int swapElementTwo = vectorOne.at(indexTwo);
+    
+    cout << swapElementOne << ", " << swapElementTwo << endl;
+    
+    if (swapElementOne > swapElementTwo){
+        indexOne = indexTwo;
+        indexTwo = indexTwo - 1;
+        cout << indexOne << " " << indexTwo << endl;
+        
+         cout << swapElementOne << ", " << swapElementTwo << endl;
+    }
+
+    for (indexOne = 0; indexOne < vectorOne.size(); ++indexOne){
+        cout << vectorOne.at(indexOne) << ", ";
+        }
+    cout << " " << endl;
+    cout << vectorOne.size() << endl;
     return 0;
 }
 
@@ -67,5 +89,6 @@ int sortVector(){
 int main (){
     
     removeDuplicates();
+    sortVector();
     
 }
